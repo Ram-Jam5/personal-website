77649d4b06df22c63918e5457f8cf2bbc2abcb1f
Ramy <edgeillusive@gmail.com>
2025-04-29 13:50:27 -0500

    Add style.css and media images.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -3,3 +3,7 @@ from .models import Project
 # Register your models here.
 
 admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'url')
+    search_fields = ('title',)
+    list_editable = ('description', 'url')
